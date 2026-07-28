0cf36a4ea5c745a3f21450ab397c341df160f5e2
BATS SÉBASTIEN <sebastien.bats@gmail.com>
2026-07-28 12:48:06 +0200

    Update exports.js

--- backend/routes/exports.js
+++ backend/routes/exports.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { 
-  exportCSV, 
-  exportGeoJSON, 
+const {
+  exportCSV,
+  exportGeoJSON,
   listExports,
-  deleteExport 
+  deleteExport
 } = require('../controllers/exportController');
 
 router.post('/csv', exportCSV);
